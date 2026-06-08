f4d2fe293a3b9a7232bc90c81c2d5d4236ea140b
Boris Branse <branseb@gmail.com>
2026-06-08 10:03:41 +0200

    init shared widget

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { TravelOrdersWidget, DEFAULT_STRAVNE_RATES, getRatesForDate, getAllCountries } from './TravelOrdersWidget'
+export type {
+    TravelOrder, TravelOrderInput, TravelOrdersWidgetProps,
+    StravneRates, StravneRatesEntry, ForeignStravneRate, StravneMeals,
+    EmployeeRecord,
+} from './TravelOrdersWidget'
